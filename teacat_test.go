cfc7fe7ac4727f135cfb91edf5129f92361c556e
hiroki_ak <hiroky@hiroki-aknoMacBook-Pro.local>
2021-07-19 23:47:25 +0900

    change test code

--- teacat_test.go
+++ teacat_test.go
@@ -7,8 +7,16 @@ func TestParseArgs(t *testing.T) {
 		giveArgs   []string
 		wontStatus int
 	}{
+		{[]string{"teacat", "-w", "teacat.go"}, 0},
+		{[]string{"teacat", "-b", "teacat.go"}, 0},
+		{[]string{"teacat", "-l", "teacat.go"}, 0},
+		{[]string{"teacat", "-c", "teacat.go"}, 0},
+		{[]string{"teacat", "-n", "teacat.go"}, 0},
 		{[]string{"teacat", "-h"}, 0},
 		{[]string{"teacat"}, 0}, // required parameters missing
+		{[]string{"teacat", "-w", "teacat.txt"}, 0},
+		{[]string{"teacat", "teacat.go"}, 1},
+		{[]string{"teacat", "-p", "teacat.txt"}, 1},
 		{[]string{"teacat", "-a"}, 1},
 	}
 	for _, td := range testdata {
